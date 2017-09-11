66ac88018de0b7f556c7c67df02fcf678b73e5f2
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-09-11 15:54:05 -0400

    repair DataFetcherTask

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -29,16 +32,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
+    private DataFetcherTask fetcherTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        fetcherTask = new DataFetcherTask();
+        fetcherTask.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +55,26 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+
+
+    private class DataFetcherTask extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... urls) {
+            if(urls.length < 1){
+                return null;
+            }
+            return Utils.fetchEarthquakeData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event result) {
+           if(result == null){
+               return;
+           }
+
+           updateUi(result);
+        }
     }
 }
